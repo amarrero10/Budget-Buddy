74c59bd14faeeef6e3dfc32daff7569cc289f0eb
Albert Marrero <albert.marrero10@gmail.com>
2023-08-22 20:04:55 -0400

    Working on front end

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,9 +4,7 @@ const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
-
 router.use("/session", sessionRouter);
-
 router.use("/users", usersRouter);
 
 router.post("/test", (req, res) => {
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" bg-slate-600">
+      <h1 className="text-white">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
