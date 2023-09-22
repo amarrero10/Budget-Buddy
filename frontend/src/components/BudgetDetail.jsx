6c91d4605029a8072f03ba5705ef02c35828fcc8
Albert Marrero <albert.marrero10@gmail.com>
2023-09-22 12:49:05 -0400

    Work on budget features

--- frontend/src/components/BudgetDetail.jsx
+++ frontend/src/components/BudgetDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import * as budgetActions from "../store/budgets";
+import { BsLink, BsPencilSquare, BsTrash3 } from "react-icons/bs";
 
 function BudgetDetail() {
   const dispatch = useDispatch();
@@ -12,8 +13,62 @@ function BudgetDetail() {
     dispatch(budgetActions.fetchBudget(id));
   }, [dispatch, id]);
 
-  console.log("SINGLE BUDGET", budget);
-  return <div>{budget.budgetName}</div>;
+  let billTotal = 0;
+  let paidBills = [];
+
+  for (let i = 0; i < budget?.Bills?.length; i++) {
+    const bill = budget.Bills[i];
+    if (bill.paid) {
+      billTotal += bill.billAmount;
+      paidBills.push(bill);
+    }
+  }
+
+  return (
+    <>
+      <table className="custom-table">
+        <thead>
+          <tr>
+            <th className="table-header">Paid</th>
+            <th className="table-header">Name</th>
+            <th className="table-header">Amount</th>
+            <th className="table-header">Due Date</th>
+            <th className="table-header">Edit</th>
+            <th className="table-header">Delete</th>
+            <th className="table-header">Payment Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {paidBills?.map((bill, index) => (
+            <tr key={bill.id} className={bill.paid ? "paid" : ""}>
+              <td>
+                <div className="bill-paid">
+                  <button className={`check-button ${bill.paid ? "checked" : ""}`}>
+                    &#10003; {/* Checkmark symbol */}
+                  </button>
+                </div>
+              </td>
+              <td>{bill?.billName}</td>
+              <td>{bill?.billAmount}</td>
+              <td>{bill?.dueDate ? bill.dueDate : ""} </td>
+
+              <td className="edit">
+                <BsPencilSquare className="bill-icon" />
+              </td>
+              <td className="delete">
+                <BsTrash3 className="bill-delete" />
+              </td>
+              <td className="edit">
+                <a href={bill.paymentLink} target="_blank" rel="noreferrer">
+                  <BsLink className="bill-icon" />
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default BudgetDetail;
